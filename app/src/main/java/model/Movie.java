71dc3fed00d04084f778392ba66c53020f9ff94a
Cécile <cecile.roger@centrale-marseille.fr>
2016-03-12 17:51:56 +0100

    Load and display a list of the movies

---
+++ app/src/main/java/model/Movie.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Movie {
+    private String title, thumbnailUrl;
+    private int year;
+    private double rating;
+    private String genre;
+
+    public Movie() {
+    }
+
+    public Movie(String name, String thumbnailUrl, int year, double rating,
+                 String genre) {
+        this.title = name;
+        this.thumbnailUrl = thumbnailUrl;
+        this.year = year;
+        this.rating = rating;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+}
